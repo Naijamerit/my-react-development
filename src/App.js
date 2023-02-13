021ac0903ebadb184b7c910909f50e60d469ff78
Naijamerit <victor.ejiogu@ust.edu.ng>
2023-02-13 11:05:02 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Button from './component/Button';
 
 function App() {
   return (
-    <div>
+    <div style={{ display: 'flex' }}>
       <div
         style={{
           border: 'solid 2px black',
@@ -12,6 +12,7 @@ function App() {
           width: '50%',
           borderRadius: '20px',
           padding: '20px',
+          marginRight: '30px',
         }}
       >
         <CardComponent cardTitle="Heading 1" />
@@ -27,6 +28,33 @@ function App() {
           fontSize="15px"
           borderRadius="5px"
           buttonText="btn1"
+        />
+      </div>
+
+      <div
+        style={{
+          border: 'solid 2px black',
+          textAlign: 'center',
+          width: '50%',
+          borderRadius: '20px',
+          padding: '20px',
+          marginLift: '30px',
+        }}
+      >
+        <CardComponent cardTitle="Heading 2" />
+        <CardComponent
+          cardText="Perspiciatis 
+          minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
+          quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus"
+        />
+        <Button
+          width="100px"
+          padding="10px"
+          backgroundColor="white"
+          border="solid"
+          fontSize="15px"
+          borderRadius="5px"
+          buttonText="btn2"
         />
       </div>
     </div>
